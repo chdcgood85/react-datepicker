chdcgood85/react-datepicker

commit d07308d3860389449e069ad766b966467d328a2c

🐛 Fix the Esc key input refocus issue by deferring the focus operation to the next event loop
- This issue is caused due to the defer blur made in the commit 36822223 is getting executed after the focus operation for the ESC key press

Close #5084

src/index.tsx
@@ -452,6 +452,14 @@ export default class DatePicker extends Component<
     }
   };
 
+  safeFocus = () => {
+    setTimeout(() => {
+      if (this.input && this.input.focus) {
+        this.input.focus({ preventScroll: true });
+      }
+    }, 0);
+  };
+
   safeBlur = () => {
     setTimeout(() => {
       if (this.input && this.input.blur) {
@@ -461,9 +469,7 @@ export default class DatePicker extends Component<
   };
 
   setFocus = () => {
-    if (this.input && this.input.focus) {
-      this.input.focus({ preventScroll: true });
-    }
+    this.safeFocus();
   };
 
   setBlur = () => {

src/test/datepicker_test.test.tsx
@@ -1983,7 +1983,7 @@ describe("DatePicker", () => {
     });
     expect(div.querySelector("input")).toBe(document.activeElement);
   });
-  it("should autoFocus the input when calling the setFocus method", () => {
+  it("should autoFocus the input when calling the setFocus method", async () => {
     const div = document.createElement("div");
     document.body.appendChild(div);
     let instance: DatePicker | null = null;
@@ -2001,7 +2001,9 @@ describe("DatePicker", () => {
     act(() => {
       instance!.setFocus();
     });
-    expect(div.querySelector("input")).toBe(document.activeElement);
+    await waitFor(() => {
+      expect(div.querySelector("input")).toBe(document.activeElement);
+    });
   });
   it("should clear preventFocus timeout id when component is unmounted", () => {
     const div = document.createElement("div");

commit 1928ff84f8fcf84a4cb142c207ef4026a148c36c

🐛 Fix the re-focus issue of the DatePicker element when the open state is controlled manually
- Defer the blur call alone to fix the issue #5084
- Fix the bug caused in the commit d07308d3860389449e069ad766b966467d328a2c

Closes #5366

src/index.tsx
@@ -452,25 +452,20 @@ export default class DatePicker extends Component<
     }
   };
 
-  safeFocus = () => {
-    setTimeout(() => {
-      this.input?.focus?.({ preventScroll: true });
-    }, 0);
-  };
-
-  safeBlur = () => {
-    setTimeout(() => {
-      this.input?.blur?.();
-    }, 0);
-  };
-
   setFocus = () => {
-    this.safeFocus();
+    this.input?.focus?.({ preventScroll: true });
   };
 
   setBlur = () => {
-    this.safeBlur();
+    this.input?.blur?.();
     this.cancelFocusInput();
+  };
+
+  deferBlur = () => {
+    requestAnimationFrame(() => {
+      console.log("reached");
+      this.setBlur();
+    });
   };
 
   setOpen = (open: boolean, skipSetBlur: boolean = false): void => {
@@ -490,7 +485,7 @@ export default class DatePicker extends Component<
               focused: skipSetBlur ? prev.focused : false,
             }),
             () => {
-              !skipSetBlur && this.setBlur();
+              !skipSetBlur && this.deferBlur();
 
               this.setState({ inputValue: null });
             },